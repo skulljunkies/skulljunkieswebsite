30458c7a7e07535eaffc97db90ee531427162010
seanpen00 <seanpen00@gmail.com>
2021-10-13 23:38:08 -0400

    added comic strips

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-let images = [{imgsrc:"media/slide1.jpeg"},{imgsrc:"media/slide2.jpeg"},{imgsrc:"media/slide3.jpeg"}]
+let images = [
+    {imgsrc:"media/slide1.jpeg"},
+    {imgsrc:"media/slide2.jpeg"},
+    {imgsrc:"media/slide3.jpeg"},
+    {imgsrc:"media/1.jpg"},
+    {imgsrc:"media/2.jpg"},
+    {imgsrc:"media/3.jpg"},
+    {imgsrc:"media/4.jpg"},
+    {imgsrc:"media/5.jpg"}
+]
 let x = 0;
 let slide0 = images[0];
 let slide1 = images[1];
 let slide2 = images[2];
 
 function rightArrow() {
-    if(x == 2) {
+    if(x == 7) {
         x = 0;
         document.getElementById("slide").src = images[0].imgsrc;
     }
@@ -17,7 +26,7 @@ function rightArrow() {
 
 function leftArrow() {
     if(x == 0) {
-        x = 2;
+        x = 7;
         document.getElementById("slide").src = images[x].imgsrc;
     }
     else {
